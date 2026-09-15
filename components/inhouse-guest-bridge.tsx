b45a23949011acb542c5126618c2b2bf9f32c6e3
Edward <edwardjacob721@yahoo.com>
2026-09-16 04:03:29 +0800

    Use MUI-style radio icons in in-house sort menu

--- components/inhouse-guest-bridge.tsx
+++ components/inhouse-guest-bridge.tsx
@@ -269,6 +269,11 @@ export function InhouseGuestBridge({ store }: { store: TransportData }) {
                         setSortOpen(false);
                       }}
                     />
+                    <svg className="inhouse-sort-radio" viewBox="0 0 24 24" width={18} height={18} fill="currentColor" aria-hidden="true" focusable="false">
+                      {sortKey === option.key
+                        ? <path d="M12 7c-2.76 0-5 2.24-5 5s2.24 5 5 5 5-2.24 5-5-2.24-5-5-5zm0-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />
+                        : <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />}
+                    </svg>
                     <span>{option.label}</span>
                   </label>
                 ))}
